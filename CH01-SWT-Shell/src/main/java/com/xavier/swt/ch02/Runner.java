43b45d7291573d5450902a68e61665125fb148b2
finchan <finchan@outlook.com>
2018-07-04 00:07:59 +0800

    SWT examples

---
+++ CH01-SWT-Shell/src/main/java/com/xavier/swt/ch02/Runner.java
@@ -0,0 +1,10 @@
+package com.xavier.swt.ch02;
+
+/**
+ * Created by Xavier on 2018-07-03.
+ */
+public class Runner {
+    public static void main(String[] args) {
+        ShellStylesExample ss = new ShellStylesExample();
+    }
+}
