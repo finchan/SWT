71208b469a9d7ec42692a29b920deceed05cd981
Xavier Ding <finchan_ding@hotmail.com>
2018-07-05 12:49:47 +0800

    SWT - Chapter 2 - Menu

--- CH01-SWT-Shell/src/main/java/com/xavier/swt/ch02/ChildShell.java
+++ CH01-SWT-Shell/src/main/java/com/xavier/swt/ch02/ChildShell.java
@@ -6,7 +6,7 @@ import org.eclipse.swt.widgets.Shell;
  * Created by Xavier on 2018/7/4.
  */
 public class ChildShell {
-    ChildShell(Shell parent, int style) {
+    public ChildShell(Shell parent, int style) {
         Shell child = new Shell(parent, style);
         child.setSize(200, 200);
         child.open();
