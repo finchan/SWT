9e75063b9fff4cc5aca85aec975da6de8f2d6f57
finchan <finchan@outlook.com>
2018-07-15 16:37:20 +0800

    Chapter 8 - SWT Combo

---
+++ CH07-SWT-Combo/src/main/java/com/xavier/swt/ch08/Runner.java
@@ -0,0 +1,10 @@
+package com.xavier.swt.ch08;
+
+/**
+ * Created by Xavier on 2018-07-15.
+ */
+public class Runner {
+    public static void main(String[] args){
+        new ComboShell();
+    }
+}
